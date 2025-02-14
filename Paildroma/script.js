fa7f62246fb5e01bf8ead7eaca9e55cdffa697fa
imnxtamess <davide.morisco.work@gmail.com>
2025-02-14 14:09:47 +0100

    edit function bug and add call isPalindrome function

--- Paildroma/script.js
+++ Paildroma/script.js
@@ -9,12 +9,21 @@ const user_word = prompt("Insert a word here to find out if it's a palindrome: "
 
 function isPalindrome(string) {
   const stringToArray = string.split(""); // Turns the user input into an array of its letters
-  const reverseArray = split_string.reverse(); // reverses the letters of the array
+  const reverseArray = stringToArray.reverse(); // reverses the letters of the array
   const arrayToString = reverseArray.join(""); // turns the reversed array into a string 
-  if (arrayTostring === string) {
+  if (arrayToString === string) {
     return isPalindrome = true
   }
   else {
     return isPalindrome = false
   }
 }
+
+// Call the isPalindrome function
+
+if (isPalindrome(user_word)) {
+  document.writeln("The word you chose is a palindrome")
+}
+else {
+  document.writeln("The word you chose isn't a palindrome")
+} 
