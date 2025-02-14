801dff5ce30268adb6442042edb100b1969398d2
imnxtamess <davide.morisco.work@gmail.com>
2025-02-14 13:54:52 +0100

    add user prompt to palindrome

--- Paildroma/script.js
+++ Paildroma/script.js
@@ -1,4 +1,8 @@
 // Palidroma
 // Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
 
+// Ask the user for an input word
 
+prompt("Insert a word here to find out if it's a palindrome: ")
+
+// isPalindrome function
