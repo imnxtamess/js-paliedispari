4427508b952fbaa06cbf232d34032f8e8f52bda0
imnxtamess <davide.morisco.work@gmail.com>
2025-02-14 13:17:26 +0100

    add prompts to pari dispari

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 // Pari e Dispari
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random(sempre da 1 a 5) per il computer(usando una funzione).Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari(usando una funzione) Dichiariamo chi ha vinto.
+
+// The user picks between even or uneven ⬇️
+
+let user_choice = prompt("Pick between even and uneven: ex:(write 'even' or 'uneven'")
+
+// The user picks a number between 1 or 5 ⬇️
+
+let user_number = prompt("Choose a number between 1 and 5: ex:(write '1' '3' etc..")
+
+// Generate a random number for the computer function() ⬇️
+
